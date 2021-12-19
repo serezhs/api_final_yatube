1a99876272f4ad18d1c4ba5d6bbbb4d70f2a2b22
Егоркин Сергей <serioga3214@gmail.com>
2021-12-20 02:16:41 +0300

    ~_~

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,56 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Follow, Group, Post
+from rest_framework import filters, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+
+from .premissions import AuthorOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+                          PostSerializer)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        new_queryset = Comment.objects.filter(post=post)
+
+        return new_queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post=get_object_or_404(
+            Post, id=self.kwargs.get('post_id'))
+        )
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        get_user = self.request.user
+        new_queryset = Follow.objects.filter(user=get_user)
+
+        return new_queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from rest_framework import routers
+
+from api.views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'api/v1/posts', PostViewSet)
+router.register(r'api/v1/groups', GroupViewSet)
+router.register(r'api/v1/follow', FollowViewSet, basename='follow')
+router.register(r'api/v1/posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('djoser.urls.jwt')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    path('', include(router.urls)),
 ]
